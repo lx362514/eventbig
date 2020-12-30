62fc980d67a043909d38fa2f70be6ab668c78a9c
luoxin <1423492393@qq.com>
2020-12-30 10:17:24 +0800

    x

--- assets/js/login.js
+++ assets/js/login.js
@@ -6,17 +6,18 @@ $(function () {
         $('.reg-box').show()
     });
     // 点击去登录的按钮
-    $('#link_login').on('click', function () {
+    $('#link_reg').on('click', function () {
         $('.login-box').show()
         $('.reg-box').hide()
     });
+    
     //  从layui中获取form对象  \s不能有空格
     // var form = layui.form;
     // var layer = layui.layer;
     const { form, layer } = layui;
     // 通过form.verify（)函数自定义校验规则
     form.verify({
-        pwd: [/^[\S]{6,12}$/, '密码必须6-12位，且不能出现空格'],
+        pwd: [/^[\S]{6,12}$/,'密码必须6-12位,且不能出现空格'],
         repwd: function (value) {
             const pwd = $('.reg-box [name=password]').val();
             if (pwd !== value) {
@@ -48,12 +49,13 @@ $(function () {
                 // 自动跳转登录界面
                 $('#link_login').click();
 
-            },
+            }
         });
     });
     // 监听表单提交事件
     $('#form_login').on('submit',(function (e) {
         e.preventDefault();
+
         $.ajax({
             url: '/api/login',
             method: 'POST',
@@ -67,9 +69,9 @@ $(function () {
                 // 跳转到后台
                 localStorage.setItem('token',res.token);  //保存至浏览器本地
                 location.href = '/index.html';  //跳转页面
-            },
+            }
 
         });
-    });
+    })
 
 });
